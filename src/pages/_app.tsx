88f0071b3faffc9a3e3463cff0ffcedddc278bbf
Bruno <brn.milano@hotmail.com>
2024-08-01 15:56:48 -0300

    Estrutura visual: - Configurando Stitches; - Estilos globais; - Cabeçalho da aplicação; - Imagens no Next.js; - Estrutura da Home; - Criando o carrossel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,25 @@
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
-import "@/styles/index.scss";
+import { Container, Header } from "@/styles/pages/app";
+import { globalStyles } from "@/styles/global";
+import LogoImg from "@/assets/logo.svg";
+import Image from "next/image";
 
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
 });
 
+globalStyles();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={LogoImg} alt="" />
+      </Header>
+
       <Component {...pageProps} />
-    </main>
+    </Container>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,62 @@
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+
+import Camiseta1 from "@/assets/camisetas/camisa-1.png";
+import Camiseta2 from "@/assets/camisetas/camisa-2.png";
+import Camiseta3 from "@/assets/camisetas/camisa-3.png";
+import Camiseta4 from "@/assets/camisetas/camisa-4.png";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={Camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={Camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={Camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={Camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
